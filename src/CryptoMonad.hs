1ce2ff0d55906ead94fe708638b4d1d6a2afe49e
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2023-11-26 16:28:25 +0100

    rewrite CryptoMonad to have only one parameter, this reduces type inference ambiguity
    before, there was a problem with inferring type of l by given types of
    (MapFst l) and (MapSnd l), now we've eliminated it and type inference
    just works

--- src/CryptoMonad.hs
+++ src/CryptoMonad.hs
@@ -83,31 +83,37 @@ data SomeIndex xs where
 data SomeMessage xs where
   SomeMessage :: InList x xs -> x -> SomeMessage xs
 
+data SomeSndMessage xs where
+  SomeSndMessage :: InList (x, y) xs -> y -> SomeSndMessage xs
+
+data SomeFstMessage xs where
+  SomeFstMessage :: InList (x, y) xs -> x -> SomeFstMessage xs
+
 padMessageIndex :: SomeMessage ts -> SomeMessage (t : ts)
 padMessageIndex (SomeMessage i' x') = SomeMessage (There i') x'
 
 -- domain-specific definitions
 
-data CryptoActions (send :: [Type]) (recv :: [Type]) a where
-    RecvAction :: (SomeMessage recv -> a) -> CryptoActions send recv a
-    SendAction :: SomeMessage send -> a -> CryptoActions send recv a
+data CryptoActions (l :: [Type]) a where
+    RecvAction :: (SomeSndMessage l -> a) -> CryptoActions l a
+    SendAction :: SomeFstMessage l -> a -> CryptoActions l a
 
-instance Functor (CryptoActions send recv) where
+instance Functor (CryptoActions l) where
     fmap f (RecvAction g) = RecvAction (f . g)
     fmap f (SendAction m a) = SendAction m $ f a
 
 -- wrappers
 
-type CryptoMonad send recv = Free (CryptoActions send recv)
+type CryptoMonad l = Free (CryptoActions l)
 
-recvAny :: CryptoMonad send recv (SomeMessage recv)
+recvAny :: CryptoMonad l (SomeSndMessage l)
 recvAny = liftF (RecvAction id)
 
 -- |Waits for message from this specific party. Until it arrives, collect all
 -- the messages from all other parties.
-recvCollecting :: InList b recv -> CryptoMonad send recv ([SomeMessage recv], b)
+recvCollecting :: InList (a, b) l -> CryptoMonad l ([SomeSndMessage l], b)
 recvCollecting i = do
-  m@(SomeMessage j x) <- recvAny
+  m@(SomeSndMessage j x) <- recvAny
   case areInListEqual i j of
     Nothing -> do
       (ms, res) <- recvCollecting i
@@ -115,7 +121,7 @@ recvCollecting i = do
     Just Refl -> pure ([], x)
 
 -- |Same as @recvCollecting@, but drops the messages from other parties.
-recvDropping :: InList b recv -> CryptoMonad send recv b
+recvDropping :: InList (a, b) l -> CryptoMonad l b
 recvDropping i = snd <$> recvCollecting i
 
 data HeteroListP2 f a (types :: [Type]) where
@@ -130,25 +136,25 @@ repackMessage (HConsP2 i is) m@(SomeMessage j x) = case areInListEqual i j of
     Just Refl -> Just $ SomeMessage Here x
     Nothing -> padMessageIndex <$> repackMessage is m
 
--- |Same as @recvDropping@, but takes a list of valid senders to get messages from
-recvOneOfDropping :: HeteroListP2 InList recv is -> CryptoMonad send recv (SomeMessage is)
-recvOneOfDropping i = do
-  m <- recvAny
-  case repackMessage i m of
-    Nothing -> recvOneOfDropping i
-    Just m' -> pure m'
+-- -- |Same as @recvDropping@, but takes a list of valid senders to get messages from
+-- recvOneOfDropping :: HeteroListP2 InList l is -> CryptoMonad l (SomeMessage (MapSnd is))
+-- recvOneOfDropping i = do
+--   m <- recvAny
+--   case repackMessage i m of
+--     Nothing -> recvOneOfDropping i
+--     Just m' -> pure m'
 
-sendSomeMess :: SomeMessage send -> CryptoMonad send recv ()
+sendSomeMess :: SomeFstMessage l -> CryptoMonad l ()
 sendSomeMess m = liftF (SendAction m ())
 
-send :: InList b send -> b -> CryptoMonad send recv ()
-send i b = sendSomeMess $ SomeMessage i b
+send :: InList (b, a) l -> b -> CryptoMonad l ()
+send i b = sendSomeMess $ SomeFstMessage i b
 
 -- usage
 
-data BobAlgo = BobAlgo (CryptoMonad [Int, Void, BobAlgo] [Bool, Void, String] Bool)
+data BobAlgo = BobAlgo (CryptoMonad [(Int, Bool), (Void, Void), (BobAlgo, String)] Bool)
 
-alg1 :: CryptoMonad [Int, Void, BobAlgo] [Bool, Void, String] Bool
+alg1 :: CryptoMonad [(Int, Bool), (Void, Void), (BobAlgo, String)] Bool
 alg1 = do str <- recvDropping charlie
           send alice $ length str
           send charlie $ BobAlgo alg1
@@ -177,20 +183,20 @@ type family MapSnd xs where
 type family Swap p where
     Swap ((,) x y) = (,) y x
 
-type CryptoMonad' people = CryptoMonad (MapFst people) (MapSnd people)
+-- type CryptoMonad' people = CryptoMonad (MapFst people) (MapSnd people)
 
-send' :: InList (a, b) l -> a -> CryptoMonad' l ()
-send' i m = send (inListFst i) m
+-- send' :: InList (a, b) l -> a -> CryptoMonad l ()
+-- send' i m = send (inListFst i) m
 
 -- heteroListP2mapSnd :: HeteroListP2 InList l is -> HeteroListP2 InList (MapSnd l) (MapSnd is)
 -- heteroListP2mapSnd HNilP2 = HNilP2
 -- heteroListP2mapSnd (HConsP2 x xs) = HConsP2 (inListSnd x) (heteroListP2mapSnd xs)
 
--- recvOneOfDropping' :: HeteroListP2 InList l is -> CryptoMonad' l (SomeMessage is)
+-- recvOneOfDropping' :: HeteroListP2 InList l is -> CryptoMonad l (SomeMessage is)
 -- recvOneOfDropping' i = recvOneOfDropping $ inListSnd i
 
-recvDropping' :: InList (a, b) l -> CryptoMonad' l b
-recvDropping' i = recvDropping $ inListSnd i
+-- recvDropping' :: InList (a, b) l -> CryptoMonad l b
+-- recvDropping' i = recvDropping $ inListSnd i
 
 inListFst :: InList ((,) a b) l -> InList a (MapFst l)
 inListFst Here = Here
@@ -200,7 +206,7 @@ inListSnd :: InList ((,) a b) l -> InList b (MapSnd l)
 inListSnd Here = Here
 inListSnd (There x) = There $ inListSnd x
 
-alg1' :: CryptoMonad' [(Int, Bool), (Void, Void), (BobAlgo, String)] Bool
+alg1' :: CryptoMonad [(Int, Bool), (Void, Void), (BobAlgo, String)] Bool
 alg1' = alg1
 
 -- |Returns @Left (x, f)@ if the underlying monad has received message x
@@ -210,36 +216,40 @@ alg1' = alg1
 --
 -- Returns @Right a@ if the simulated computation exited successfully (and all
 -- arrived messages were ok) with result @a@.
-hidingRecvParty
-  :: CryptoMonad send recv a
-  -> CryptoMonad send (x:recv) (Either (x, CryptoMonad send recv a) a)
-hidingRecvParty (Pure x) = Pure $ Right x
-hidingRecvParty y@(Free (RecvAction f))
+hidingParty
+  :: CryptoMonad l a
+  -> CryptoMonad ((x, y):l) (Either (y, CryptoMonad l a) a)
+hidingParty (Pure x) = Pure $ Right x
+hidingParty y@(Free (RecvAction f))
   = Free
   $ RecvAction
   $ \case
-    (SomeMessage Here x) -> Pure $ Left (x, y)
-    (SomeMessage (There i) x) -> hidingRecvParty $ f (SomeMessage i x)
-hidingRecvParty (Free (SendAction m a))
+    (SomeSndMessage Here x) -> Pure $ Left (x, y)
+    (SomeSndMessage (There i) x) -> hidingParty $ f (SomeSndMessage i x)
+hidingParty (Free (SendAction (SomeFstMessage i m) a))
   = Free
-  $ SendAction m
-  $ hidingRecvParty a
+  $ SendAction (SomeFstMessage (There i) m)
+  $ hidingParty a
 
 -- Interpretation of the CryptoMonad
 
-runSTM :: HeteroList STM.TChan send
-    -> HeteroList STM.TChan receive
-    -> CryptoMonad send receive a
+data TwoChans t where
+  TwoChans :: STM.TChan a -> STM.TChan b -> TwoChans (a, b)
+
+runSTM :: HeteroList TwoChans l
+    -> CryptoMonad l a
     -> IO a
-runSTM s r = \case
+runSTM l = \case
   Pure x -> pure x
   Free (RecvAction f) -> do
-    let chans = homogenize (\i c -> SomeMessage i <$> STM.readTChan c) r
+    let chans = homogenize (\i (TwoChans _ r) -> SomeSndMessage i <$> STM.readTChan r) l
     m <- STM.atomically $ msum chans
-    runSTM s r $ f m
-  Free (SendAction (SomeMessage i m) a) -> do
-    STM.atomically $ STM.writeTChan (heteroListGet s i) m
-    runSTM s r a
+    runSTM l $ f m
+  Free (SendAction (SomeFstMessage i m) a) -> do
+    STM.atomically $ do
+      let (TwoChans s _) = heteroListGet l i
+      STM.writeTChan s m
+    runSTM l a
 
 type VoidInterface = (Void, Void)
 type AliceBobInterface = (String, Int)
@@ -247,20 +257,17 @@ type AliceBobInterface = (String, Int)
 -- aliceName = Here
 
 alice :: (l ~ '[VoidInterface, AliceBobInterface])
-      => InList AliceBobInterface l -> CryptoMonad' l Int
+      => InList AliceBobInterface l -> CryptoMonad l Int
 alice bobName = do
-  let bobRecv = inListSnd bobName
-  send' bobName "alice to bob string"
-  m <- recvOneOfDropping (HConsP2 bobRecv HNilP2)
-  case m of
-    SomeMessage Here x -> pure x
-    SomeMessage (There contra) _ -> case contra of
+  -- let bobRecv = inListSnd bobName
+  send bobName "alice to bob string"
+  recvDropping bobName
 
 bob :: (l ~ '[Swap AliceBobInterface, VoidInterface])
-    => InList (Swap AliceBobInterface) l -> CryptoMonad' l String
+    => InList (Swap AliceBobInterface) l -> CryptoMonad l String
 bob aliceName = do
-  s <- recvDropping' aliceName
-  send' aliceName $ length s
+  s <- recvDropping aliceName
+  send aliceName $ length s
   pure $ "got from Alice " ++ show s
 
 test2STM :: IO (Int, String)
@@ -268,40 +275,38 @@ test2STM = do
     aToBChan <- STM.newTChanIO
     bToAChan <- STM.newTChanIO
     voidChan <- STM.newTChanIO
-    let aliceSend = HCons voidChan (HCons aToBChan HNil)
-    let aliceRecv = HCons voidChan (HCons bToAChan HNil)
-    let bobSend = HCons bToAChan (HCons voidChan HNil)
-    let bobRecv = HCons aToBChan (HCons voidChan HNil)
-    aliceA <- A.async $ runSTM aliceSend aliceRecv $ alice (There Here)
-    bobA <- A.async $ runSTM bobSend bobRecv $ bob Here
+    let aliceCh = HCons (TwoChans voidChan voidChan) (HCons (TwoChans aToBChan bToAChan) HNil)
+    let bobCh = HCons (TwoChans bToAChan aToBChan) (HCons (TwoChans voidChan voidChan) HNil)
+    aliceA <- A.async $ runSTM aliceCh $ alice (There Here)
+    bobA <- A.async $ runSTM bobCh $ bob Here
     A.waitBoth aliceA bobA
 
 -- Single-threaded Cooperative Multitasking Interpretation of the Monad.
 --
 -- This is defined by the original UC paper
 
-data Thread send recv a
+data Thread l a
   = ThDone a
-  | ThRunning (SomeMessage recv -> Free (CryptoActions send recv) a)
+  | ThRunning (SomeSndMessage l -> Free (CryptoActions l) a)
 
 -- |Start a new thread and run it until it terminates or tries to recv. Collect
 -- messages that it tries to send.
-newThread :: CryptoMonad send recv a
-          -> (Thread send recv a, [SomeMessage send])
+newThread :: CryptoMonad l a
+          -> (Thread l a, [SomeFstMessage l])
 newThread (Pure x) = (ThDone x, [])
 newThread (Free (RecvAction a)) = (ThRunning a, [])
 newThread (Free (SendAction m a)) = second (m:) $ newThread a
 
-deliverThread :: SomeMessage recv
-              -> Thread send recv a
-              -> (Thread send recv a, [SomeMessage send])
+deliverThread :: SomeSndMessage l
+              -> Thread l a
+              -> (Thread l a, [SomeFstMessage l])
 deliverThread _ t@(ThDone _) = (t, [])
 deliverThread m (ThRunning a) = case a m of
   Pure x -> (ThDone x, [])
   a' -> newThread a'
 
--- runCoop2PC :: CryptoMonad' [(Void, Void), (a, b)] c
---            -> CryptoMonad' [(b, a), (Void, Void)] d
+-- runCoop2PC :: CryptoMonad [(Void, Void), (a, b)] c
+--            -> CryptoMonad [(b, a), (Void, Void)] d
 --            -> (Maybe c, Maybe d)
 -- runCoop2PC p1 p2 = helper (t1, t2, map Left m1 ++ map Right m2)
 --   where
