0c96dec1547566fe275aa7ba87c02a9f31ba99b2
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-07-04 00:22:57 +0500

    add a few examples

--- src/GADTApproach.hs
+++ src/GADTApproach.hs
@@ -22,6 +22,7 @@ module GADTApproach where
 
 import Prelude hiding ((>>=), return)
 import Control.XFreer
+import Control.XMonad
 import qualified Control.XMonad.Do as M
 
 import Data.Kind (Type)
@@ -48,6 +49,20 @@ test :: String -> CryptoMonad True True String
 test s = M.do
   send s
   recv
+
+data SBool (a :: Bool) where
+  STrue :: SBool True
+  SFalse :: SBool False
+
+data UnknownAfter bef a = forall b. UnknownAfter (SBool b) (CryptoMonad bef b a)
+
+-- maybeSends :: Bool -> (forall b. CryptoMonad True b (SBool b) -> a) -> a
+-- maybeSends True cont = cont $ send "hello" >>: xreturn SFalse
+-- maybeSends False cont = cont $ xreturn STrue
+
+maybeSends :: Bool -> UnknownAfter True ()
+maybeSends True = UnknownAfter SFalse $ send "hello"
+maybeSends False = UnknownAfter STrue $ pure ()
 
 -- testFail :: CryptoMonad False False ()
 -- testFail = send "hey"
