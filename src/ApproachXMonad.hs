1048e977be7ff6f3e1a5f9532a7250aabcd8e61e
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-07-04 21:22:34 +0500

    add list of channels to the indexed monad

--- src/ApproachXMonad.hs
+++ src/ApproachXMonad.hs
@@ -5,40 +5,63 @@ import Control.XFreer
 import Control.XMonad
 import qualified Control.XMonad.Do as M
 
+import Data.Type.Equality ((:~:)(Refl))
+
 import HeterogenousList
 
 import Data.Kind (Type)
 
-data CryptoActions (bef :: Bool) (aft :: Bool) (a :: Type) where
-  RecvAction :: (String -> a) -> CryptoActions False True a
-  SendAction :: String -> a -> CryptoActions True False a
-  RandAction :: (Bool -> a) -> CryptoActions x x a
-  PrintAction :: String -> a -> CryptoActions x x a
+data CryptoActions (l :: [Type]) (bef :: Bool) (aft :: Bool) (a :: Type) where
+  RecvAction :: (SomeSndMessage l -> a) -> CryptoActions l False True a
+  SendAction :: SomeFstMessage l -> a -> CryptoActions l True False a
+  WakeAction :: InList (x, y) l -> a -> CryptoActions l True False a
+  RandAction :: (Bool -> a) -> CryptoActions l x x a
+  PrintAction :: String -> a -> CryptoActions l x x a
 
-instance Functor (CryptoActions bef aft) where
+instance Functor (CryptoActions l bef aft) where
   fmap f (RecvAction cont) = RecvAction $ f . cont
   fmap f (SendAction m x) = SendAction m $ f x
+  fmap f (WakeAction m x) = WakeAction m $ f x
   fmap f (RandAction cont) = RandAction $ f . cont
   fmap f (PrintAction m x) = PrintAction m $ f x
 
-type CryptoMonad bef aft = XFree CryptoActions bef aft
+type CryptoMonad l bef aft = XFree (CryptoActions l) bef aft
 
-recv :: CryptoMonad False True String
-recv = xfree $ RecvAction id
+recvAny :: CryptoMonad l False True (SomeSndMessage l)
+recvAny = xfree $ RecvAction id
 
-send :: String -> CryptoMonad True False ()
-send m = xfree $ SendAction m ()
+sendMess :: SomeFstMessage l -> CryptoMonad l True False ()
+sendMess m = xfree $ SendAction m ()
 
-rand :: CryptoMonad x x Bool
+send :: InList (x, y) l -> x -> CryptoMonad l True False ()
+send i m = sendMess $ SomeFstMessage i m
+
+wake :: InList (x, y) l -> CryptoMonad l True False ()
+wake i = xfree $ WakeAction i ()
+
+recv :: InList (x, y) l -> CryptoMonad l False True y
+recv i = M.do
+  SomeSndMessage j m <- recvAny
+  case areInListEqual i j of
+    Just Refl -> pure m
+    Nothing -> M.do
+      wake j
+      recv i
+
+-- sendSync :: InList (x, y) l -> x -> CryptoMonad l True True y
+-- sendSync i m = send i m >> recv
+
+rand :: CryptoMonad l x x Bool
 rand = xfree $ RandAction id
 
-print :: String -> CryptoMonad x x ()
+print :: String -> CryptoMonad l x x ()
 print m = xfree $ PrintAction m ()
 
-test :: String -> CryptoMonad True True String
-test s = M.do
-  send s
-  recv
+-- |Sends String s to the given channel, waits for the other side to repond with
+test :: String -> InList (String, Int) l -> CryptoMonad l True True Int
+test s chan = M.do
+  send chan s
+  recv chan
 
 data SBool (a :: Bool) where
   STrue :: SBool True
@@ -47,19 +70,21 @@ data SBool (a :: Bool) where
 -- |Allows you to express computations that may leave write token in either of
 -- the two states. But the decision on which state to leave the monad in must
 -- not be based on side-effects.
-data PackWT bef x = forall aft. SomeWT (SBool aft) (CryptoMonad bef aft x)
+data PackWT l bef x = forall aft. SomeWT (SBool aft) (CryptoMonad l bef aft x)
 
 -- |Hide the given aft index from the type.
-packWT :: SBool aft -> CryptoMonad bef aft x -> PackWT bef x
+packWT :: SBool aft -> CryptoMonad l bef aft x -> PackWT l bef x
 packWT = SomeWT
 
-maybeSends :: Bool -> PackWT True ()
+-- |Wakes up whoever is on the other end of first channel if the passed
+-- argument was True
+maybeSends :: Bool -> PackWT ((x, y) : l) True ()
 maybeSends True = packWT SFalse $ M.do
-  send "hello"
+  wake Here
 maybeSends False = packWT STrue $ M.do
   pure ()
 
--- testFail :: CryptoMonad False False ()
+-- testFail :: CryptoMonad l False False ()
 -- testFail = send "hey"
 
 hey :: IO ()
