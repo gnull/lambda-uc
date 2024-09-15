d73ec496d1ecba2ef5934db4b3fa0c63daec3cd7
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-09-15 21:38:52 +0200

    Make fork work

--- src/UCHS/Monad/InterT/Eval/Async.hs
+++ src/UCHS/Monad/InterT/Eval/Async.hs
@@ -4,6 +4,9 @@ module UCHS.Monad.InterT.Eval.Async where
 
 import Data.Either.Extra (mapLeft)
 
+import Control.XMonad
+import qualified Control.XMonad.Do as M
+
 import UCHS.Monad
 import UCHS.Types
 
@@ -12,6 +15,7 @@ data ForkIndexCompT (befFst :: Index) (befSnd :: Index) where
   ForkIndexCompFst :: ForkIndexCompT NextSend NextRecv
   ForkIndexCompSnd :: ForkIndexCompT NextRecv NextSend
 
+type ForkIndexComp :: Index -> Index -> Constraint
 class ForkIndexComp befFst befSnd where
   getIndexStartCompPrf :: ForkIndexCompT befFst befSnd
 
@@ -28,6 +32,23 @@ type family ForkIndexOr bef bef' where
   ForkIndexOr NextRecv NextSend = NextSend
   ForkIndexOr NextRecv NextRecv = NextRecv
 
+data MayOnlyReturnAfterRecvT (i :: Index) (a :: Type) where
+  MayOnlyReturnVoid :: MayOnlyReturnAfterRecvT i Void
+  MayOnlyReturnType :: MayOnlyReturnAfterRecvT NextSend a
+
+type family ChooseRet i i' a a' where
+  ChooseRet NextRecv NextRecv _ _ = Void
+  ChooseRet NextSend _ a _ = a
+  ChooseRet _ NextSend _ a' = a'
+
+type MayOnlyReturnAfterRecv :: Index -> Type -> Constraint
+class MayOnlyReturnAfterRecv i a where
+  mayOnlyReturnAfterRecvPrf :: MayOnlyReturnAfterRecvT i a
+instance MayOnlyReturnAfterRecv i Void where
+  mayOnlyReturnAfterRecvPrf = MayOnlyReturnVoid
+instance MayOnlyReturnAfterRecv NextSend a where
+  mayOnlyReturnAfterRecvPrf = MayOnlyReturnType
+
 chanFromConcat :: forall (ach :: [(Type, Type)]) ach' x y.
                KnownLenT ach
                -> Chan x y (Concat ach ach')
@@ -39,25 +60,61 @@ chanFromConcat = \case
     There ch -> mapLeft There $ chanFromConcat rest ch
 
 fstToConcat :: forall ach ach' x y.
-               Chan x y ach
-            -> KnownLenT ach
+               KnownLenT ach
+            -> Chan x y ach
             -> Chan x y (Concat ach ach')
-fstToConcat Here _ = Here
-fstToConcat (There rest) (KnownLenS n) = There $ fstToConcat @_ @(ach') rest n
+fstToConcat _ Here = Here
+fstToConcat (KnownLenS n) (There rest) = There $ fstToConcat @_ @(ach') n rest
 
-sndToConcat :: KnownLenT ach
+sndToConcat :: forall ach ach' x y.
+               KnownLenT ach
             -> Chan x y ach'
             -> Chan x y (Concat ach ach')
 sndToConcat KnownLenZ = id
-sndToConcat (KnownLenS rest) = There . sndToConcat rest
+sndToConcat (KnownLenS n) = There . sndToConcat n
 
-fork :: forall m ach ach' bef bef' aft aft' a b.
-        (ForkIndexComp bef bef', ForkIndexComp aft aft')
+fork :: forall m ach ach' bef bef' aft aft' a a'.
+        ( Monad m
+        , ForkIndexComp bef bef'
+        , ForkIndexComp aft aft'
+        , MayOnlyReturnAfterRecv aft a
+        , MayOnlyReturnAfterRecv aft' a'
+        )
      => KnownLenT ach
      -> AsyncT m ach bef aft a
-     -> AsyncT m ach' bef' aft' b
-     -> AsyncT m (Concat ach ach') (ForkIndexOr bef bef') (ForkIndexOr aft aft') (a, b)
+     -> AsyncT m ach' bef' aft' a'
+     -> AsyncT m (Concat ach ach') (ForkIndexOr bef bef') (ForkIndexOr aft aft') (ChooseRet aft aft' a a')
 fork prf l r = case getIndexStartCompPrf @bef @bef' of
-  ForkIndexCompNone -> undefined
-  ForkIndexCompFst -> undefined
-  ForkIndexCompSnd -> undefined
+  ForkIndexCompNone -> M.do
+    SomeSndMessage i m <- recvAny
+    case chanFromConcat @ach @ach' prf i of
+      Left i' -> lift (runTillRecv (SomeSndMessage i' m) l) >>=: \case
+        RrRecv l' -> fork prf l' r
+        RrCall contra _ _ -> case contra of {}
+        RrHalt res -> case mayOnlyReturnAfterRecvPrf @aft @a of
+          MayOnlyReturnVoid -> case res of {}
+      Right i' -> lift (runTillRecv (SomeSndMessage i' m) r) >>=: \case
+        RrRecv r' -> fork prf l r'
+        RrCall contra _ _ -> case contra of {}
+        RrHalt res -> case mayOnlyReturnAfterRecvPrf @aft' @a' of
+          MayOnlyReturnVoid -> case res of {}
+  ForkIndexCompFst ->
+    lift (runTillSend l) >>=: \case
+      SrSend (SomeFstMessage i m) cont -> M.do
+        send (fstToConcat @ach @ach' prf i) m
+        fork prf cont r
+      SrCall contra _ _ -> case contra of {}
+      SrHalt res -> case mayOnlyReturnAfterRecvPrf @aft @a of
+        MayOnlyReturnVoid -> case res of {}
+        MayOnlyReturnType -> case getIndexStartCompPrf @aft @aft' of
+          ForkIndexCompFst -> xreturn res
+  ForkIndexCompSnd ->
+    lift (runTillSend r) >>=: \case
+      SrSend (SomeFstMessage i m) cont -> M.do
+        send (sndToConcat @ach @ach' prf i) m
+        fork prf l cont
+      SrCall contra _ _ -> case contra of {}
+      SrHalt res -> case mayOnlyReturnAfterRecvPrf @aft' @a' of
+        MayOnlyReturnVoid -> case res of {}
+        MayOnlyReturnType -> case getIndexStartCompPrf @aft @aft' of
+          ForkIndexCompSnd -> xreturn res
