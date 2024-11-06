7c646283990ffdc9f7349e9bb43b59850dcce5cb
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-11-06 17:24:50 +0100

    Add dropHList and move definitions to Data.HList

--- src/Data/HList.hs
+++ src/Data/HList.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Data.HList where
 
 import Prelude hiding ((!!))
 
-import Data.Kind (Type)
+import Data.Kind (Type, Constraint)
 import Data.Type.Equality ((:~:)(Refl), TestEquality(..))
+
+import Control.Arrow (first, second)
 
 -- * Dependent pointer into a list
 
@@ -102,6 +106,178 @@ type HList :: forall a. (a -> Type) -> [a] -> Type
 data HList f (types :: [a]) where
     HNil :: HList f '[]
     HCons :: f t -> HList f ts -> HList f (t : ts)
+
+-- |Signleton type to express the list structure (length) but not the contents.
+data KnownLenD :: forall a. [a] -> Type where
+  KnownLenZ :: KnownLenD '[]
+  KnownLenS :: forall a (x :: a) (l :: [a]). KnownLenD l -> KnownLenD (x : l)
+
+data KnownPortD p where
+  KnownPortD :: KnownPortD (P x y)
+
+-- |Class of list values for which their length is known at compile time.
+type KnownLen :: forall a. [a] -> Constraint
+class KnownLen l where
+  getKnownLenPrf :: KnownLenD l
+
+instance KnownLen '[] where
+  getKnownLenPrf = KnownLenZ
+
+instance KnownLen xs => KnownLen (x:xs) where
+  getKnownLenPrf = KnownLenS $ getKnownLenPrf @_ @xs
+
+data PortsLenD (ports :: [Port]) where
+  PortsLenZ :: PortsLenD '[]
+  PortsLenS :: PortsLenD l -> PortsLenD (P x y : l)
+
+data SameLenD :: forall a b. [a] -> [b] -> Type where
+  SameLenNil :: SameLenD '[] '[]
+  SameLenCons :: SameLenD l l' -> SameLenD (x:l) (x':l')
+
+type SameLen :: forall a b. [a] -> [b] -> Constraint
+class SameLen l l' where
+  proveSameLength :: SameLenD l l'
+
+instance SameLen '[] '[] where
+  proveSameLength = SameLenNil
+
+instance SameLen l l' => SameLen (x:l) (x':l') where
+  proveSameLength = SameLenCons proveSameLength
+
+type Concat :: forall a. [a] -> [a] -> [a]
+type family Concat xs ys where
+  Concat '[] ys = ys
+  Concat (x:xs) ys = x:Concat xs ys
+
+-- |Proof of `p ++ (p' ++ s') == (p ++ p') ++ s'`.
+concatAssocPrf :: forall p p' s' l s.
+                  ListSplitD l p s
+               -> (Concat p (Concat p' s')) :~: (Concat (Concat p p') s')
+concatAssocPrf = \case
+  SplitHere -> Refl
+  SplitThere i -> case concatAssocPrf @_ @p' @s' i of
+    Refl -> Refl
+
+concatInjPrf :: forall xs ys ys'.
+             Concat xs ys :~: Concat xs ys'
+             -> KnownLenD xs
+             -> ys :~: ys'
+concatInjPrf Refl = \case
+  KnownLenZ -> Refl
+  KnownLenS rest -> concatInjPrf Refl rest
+
+-- |Statement @`ListSplitD` l p s@ says that @l = p ++ s@, i.e. that @l@ can be
+-- cut into prefix @p@ and suffix @s@.
+--
+-- Structure-wise, this is just a pointer, like `InList`, but it carries
+-- different information on type-level.
+data ListSplitD :: forall a. [a] -> [a] -> [a] -> Type where
+  SplitHere :: ListSplitD l '[] l
+  SplitThere :: ListSplitD l p s -> ListSplitD (x:l) (x:p) s
+
+type ListSplitConcat p s = ListSplitD (Concat p s) p s
+
+class ListSplit l p s where
+  getListSplit :: ListSplitD l p s
+
+instance (KnownLen p, l ~ Concat p s) => ListSplit l p s where
+  getListSplit = getListSplit' getKnownLenPrf
+
+getListSplit' :: KnownLenD p
+              -> ListSplitD (Concat p s) p s
+getListSplit' = \case
+    KnownLenZ -> SplitHere
+    KnownLenS i -> SplitThere $ getListSplit' i
+
+listSplitConcat :: ListSplitD l p s
+                -> Concat p s :~: l
+listSplitConcat = \case
+  SplitHere -> Refl
+  SplitThere i -> case listSplitConcat i of
+    Refl -> Refl
+
+listSplitAdd :: ListSplitD l p s
+             -> ListSplitD s p' s'
+             -> ListSplitD l (Concat p p') s'
+listSplitAdd = \case
+  SplitHere -> id
+  SplitThere i -> \j -> SplitThere $ listSplitAdd i j
+
+listConcatSplit :: ListSplitD l p s
+                -> ListSplitD (Concat p s') p s'
+listConcatSplit SplitHere = SplitHere
+listConcatSplit (SplitThere i) = SplitThere $ listConcatSplit i
+
+listSplitPopSuffix :: ListSplitD (Concat p (x:s)) p (x:s)
+                   -> ListSplitD (Concat p s) p s
+listSplitPopSuffix = \case
+  SplitHere -> SplitHere
+  SplitThere i -> SplitThere $ listSplitPopSuffix i
+
+listSplitSubst :: ListSplitD l' p' (s:rest)
+               -> ListSplitConcat p' (f:rest)
+listSplitSubst = \case
+  SplitHere -> SplitHere
+  SplitThere i -> SplitThere $ listSplitSubst i
+
+listSplitSwap :: ListSplitD l p (f:l')
+              -> ListSplitD l' p' (s:rest)
+              -> ( ListSplitConcat p (s:(Concat p' (f:rest)))
+                 , ListSplitConcat p' (f:rest)
+                 )
+listSplitSwap = \case
+  SplitHere -> \p -> (SplitHere, listSplitSubst p)
+  SplitThere i -> \p -> first SplitThere $ listSplitSwap i p
+
+listSplitSuff2 :: ListSplitD l p (f:l')
+               -> ListSplitD l' p' (s:rest)
+               -> ( ListSplitConcat p (Concat p' rest)
+                  , ListSplitConcat p' rest
+                  )
+listSplitSuff2 = \case
+  SplitHere -> \p -> case listSplitConcat p of
+    Refl -> (SplitHere, listSplitPopSuffix p)
+  SplitThere i -> \p -> first SplitThere $ listSplitSuff2 i p
+
+
+-- instance ListSplit l '[] l where
+--   getListSplit = SplitHere
+-- instance ListSplit l p s => ListSplit (x:l) (x:p) s where
+--   getListSplit = SplitThere getListSplit
+
+pattern Split0 :: ListSplitD l '[] l
+pattern Split0 = SplitHere
+{-# COMPLETE Split0 #-}
+
+pattern Split1 :: ListSplitD (x0 : l) '[x0] l
+pattern Split1 = SplitThere Split0
+{-# COMPLETE Split1 #-}
+
+pattern Split2 :: ListSplitD (x0 : x1 : l)
+                            (x0 : '[x1]) l
+pattern Split2 = SplitThere Split1
+{-# COMPLETE Split2 #-}
+
+pattern Split3 :: ListSplitD (x0 : x1 : x2 : l)
+                            (x0 : x1 : '[x2]) l
+pattern Split3 = SplitThere Split2
+{-# COMPLETE Split3 #-}
+
+pattern Split4 :: ListSplitD (x0 : x1 : x2 : x3 : l)
+                            (x0 : x1 : x2 : '[x3]) l
+pattern Split4 = SplitThere Split3
+{-# COMPLETE Split4 #-}
+
+pattern Split5 :: ListSplitD (x0 : x1 : x2 : x3 : x4 : l)
+                            (x0 : x1 : x2 : x3 : '[x4]) l
+pattern Split5 = SplitThere Split4
+{-# COMPLETE Split5 #-}
+
+hlistDrop :: ListSplitD l p s
+          -> HList f l
+          -> HList f s
+hlistDrop SplitHere xs = xs
+hlistDrop (SplitThere i) (HCons _ xs) = hlistDrop i xs
 
 pattern HListMatch0 :: HList f '[]
 pattern HListMatch0 = HNil
