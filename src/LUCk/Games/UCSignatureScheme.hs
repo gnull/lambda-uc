4b91d57ec796213d98d43dc4886a406e4fa864ae
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-11-23 01:35:22 +0100

    refactor

--- src/LUCk/Games/UCSignatureScheme.hs
+++ src/LUCk/Games/UCSignatureScheme.hs
@@ -11,6 +11,8 @@ import LUCk.Syntax
 import LUCk.UC
 import LUCk.UC.Core
 
+import Data.Type.Equality
+
 import LUCk.Games.SignatureScheme (SpSignatureScheme, SignatureScheme(..))
 
 import qualified Data.Map as Map
@@ -41,35 +43,39 @@ pattern SidMess :: sid -> HListPair '[sid] '[]
 pattern SidMess sid = (HCons (Identity sid) HNil, HNil)
 {-# COMPLETE SidMess #-}
 
+recv :: PortInList x y ports
+     -> (forall i. AsyncT ports NextSend i Void)
+     -> AsyncT ports NextRecv NextSend y
+recv i h = recvAny >>=: \case
+  SomeRxMess j m -> case testEquality i j of
+    Just Refl -> xreturn m
+    Nothing -> h >>=: (\contra -> case contra of {})
+
 signatureIF :: SingleSidIdeal SignSid
                               (HListPort (SignatureScheme String String String String) Started)
                               (HListPort (SignReq String String String) (SignResp String String String))
                               '[]
 signatureIF (SidMess sid) = M.do
-    recvAny >>=: \case
-      SomeRxMess Here (PidMess _ contra) -> case contra of {}
-      SomeRxMess (There Here) (PidMess pid _) -> M.do
-        -- adversary has sent us a message before we asked, ignore
-        send Here (PidMess pid ())
-        signatureIF (SidMess sid)
-      SomeRxMess (There2 Here) (PidMess pid req) -> case req of
-        KGen -> M.do
-          -- we got our first request, let's ask adversary for algorithms
-          send (There Here) (PidMess pid Started)
-          recvAny >>=: \case
-            SomeRxMess Here (HNil, HListMatch2 _ contra) -> case runIdentity contra of {}
-            SomeRxMess (There Here) (HNil, HListMatch2 _ (Identity scheme)) -> M.do
-              (sk, pk) <- xlift $ sigKey scheme
-              send (There2 Here) (PidMess pid $ RespKGen pk)
-              helper scheme [] sk pk
-            SomeRxMess (There2 _) _ -> M.do
-              send Here (PidMess pid ())
-              signatureIF (SidMess sid)
-        _ -> M.do
-          send Here (PidMess pid ())
-          signatureIF (SidMess sid)
-      SomeRxMess (There3 contra) _ -> case contra of {}
+    (scheme, sk, pk) <- init
+    helper scheme [] sk pk
   where
+    init = M.do
+      let handle = M.do
+            send Here (PidMess "" ())
+            init
+          myRecv i = recv i $ undefined
+      tmp <- myRecv (There2 Here)
+      let (PidMess pid req) = tmp
+      case req of
+        KGen -> M.do
+          send (There Here) (PidMess pid Started)
+          tmp <- myRecv (There Here)
+          let (PidMess _ scheme) = tmp
+          (sk, pk) <- xlift $ sigKey scheme
+          send (There2 Here) (PidMess pid $ RespKGen pk)
+          xreturn (scheme, sk, pk)
+        _ -> handle
+
     helper :: SignatureScheme String String String String
            -> [(String, String, String, Bool)]
            -> String
@@ -101,7 +107,8 @@ signatureIF (SidMess sid) = M.do
             helper scheme ((m, sig, pk, True):trace) sk pk
           Ver pk' m sig -> M.do
             let verd = sigVer scheme pk m sig
-            let resp = RespVer verd -- TODO: check more conditions here
+            let resp = RespVer verd
+            -- TODO: check more conditions here
             send (There2 Here) (PidMess pid resp)
             let trace' = case resp of
                   RespVer b -> (m, sig, pk, b) : trace
